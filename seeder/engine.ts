ea2e2867244340e9854b6e1bfdfc4bc544f7b494
Skripko Andrey <itu.ccb@gmail.com>
2018-02-08 17:25:31 +0300

    seeding fix

--- seeder/engine.ts
+++ seeder/engine.ts
@@ -2,21 +2,26 @@ import * as csv from 'csvtojson';
 import { AEntityBase } from '../server/common/entity/base.entity';
 import { AServiceBase } from '../server/common/service/base.service';
 
-interface SeederItem {
+interface SeederItem extends SourceItem {
+  queue: any[];
+}
+
+export interface SourceItem {
   service: AServiceBase<AEntityBase>;
   filePath: string;
-  queue: any[];
 }
 
 class DatabaseSeeder {
   private _queue: SeederItem[] = [];
   private _isSeeding: boolean = false;
+  private _source: SourceItem[];
 
-  public addSeed(service: AServiceBase<AEntityBase>, filePath: string) {
-    this._parse({ service, filePath, queue: [] });
+  public addSource(source: SourceItem[]) {
+    this._source = source;
+    this._source.forEach((item, index) => this._parse({ ...item, queue: [] }, index));
   }
 
-  private _parse(item: SeederItem) {
+  private _parse(item: SeederItem, index: number) {
     const queue = [];
     csv({ delimiter: ';', ignoreEmpty: true })
       .fromFile(item.filePath)
@@ -30,8 +35,9 @@ class DatabaseSeeder {
       })
       .on('done', e => {
         item.queue = queue;
-        this._queue.push(item);
-        this._seed();
+        this._queue[index] = item;
+        console.log('PARSE DONE: ' + item.filePath);
+        if (this._source.length === this._queue.filter(i => i).length) this._seed();
       });
   }
 
@@ -39,20 +45,26 @@ class DatabaseSeeder {
     if (this._isSeeding && !force) return;
     this._isSeeding = true;
     const seeder = this._queue.shift();
+    console.log('START SEED: ' + seeder.filePath);
     try {
       if ((await seeder.service.get()).length) return this._seedingFinish();
       await seeder.service.post(seeder.queue as any);
 
       console.log(`SEED DONE: ${seeder.filePath}`);
+      this._seedingFinish();
     } catch (error) {
       console.log(`SEED ERROR: ${seeder.filePath}`, error);
+      // this._stopSeeding();
     }
-    this._seedingFinish();
   }
 
   private _seedingFinish() {
     if (this._queue.length) setTimeout(() => this._seed(true), 200);
     else this._isSeeding = false;
+  }
+
+  private _stopSeeding() {
+
   }
 }
 
--- seeder/index.ts
+++ seeder/index.ts
@@ -6,7 +6,7 @@ import { readdir } from 'fs';
 import { join } from 'path';
 import { AServiceBase } from '../server/common/service/index';
 import { AEntityBase } from '../server/common/entity/base.entity';
-import { databaseSeeder } from './engine';
+import { databaseSeeder, SourceItem } from './engine';
 import { config } from '../config/index';
 
 (async function run() {
@@ -16,14 +16,12 @@ import { config } from '../config/index';
   const csvPath = join(__dirname, 'csv4seed');
   const fileList = (await promisify(readdir)(csvPath)).map(file => file.replace('.csv', ''));
 
-  const seederList = entityMap
+  const seederList: SourceItem[] = entityMap
     .filter(e => fileList.indexOf(e.name) !== -1)
     .map(e => {
       const repo = connection.getRepository(e.entity);
       return { filePath: join(csvPath, e.name + '.csv'), service: new AServiceBase<AEntityBase>(repo) };
     });
 
-  seederList.forEach(item => {
-    databaseSeeder.addSeed(item.service, item.filePath);
-  });
+  databaseSeeder.addSource(seederList);
 }());
